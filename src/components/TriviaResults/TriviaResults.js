01c5e3c4d94eb5fe510b9a4241e15e0c31c3a6e5
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-10-29 17:43:41 -0400

    finished unit testing for TriviaResults

--- src/components/TriviaResults/TriviaResults.js
+++ src/components/TriviaResults/TriviaResults.js
@@ -6,15 +6,15 @@ export default function TriviaResults({ resetTrivia }) {
 
   return (
     <>
-      <h3>You answered {state.correctQuestionCount} out of 10 correctly!</h3>
+      <h3 data-testid="results-message">You answered {state.correctQuestionCount} out of 10 correctly!</h3>
       {state.correctQuestionCount !== 10 ? (
-        <p>There's room for improvment! Let's give it another shot!</p>
+        <p data-testid="partial-correct-message">There's room for improvement! Let's give it another shot!</p>
       ) : (
-        <p>
+        <p data-testid="all-correct-message">
           Amazing, you got all {state.correctQuestionCount} questions right!
         </p>
       )}
-      <button className="btn btn-primary" onClick={resetTrivia}>
+      <button data-testid="play-again-button" className="btn btn-primary" onClick={resetTrivia}>
         Play Again
       </button>
     </>
